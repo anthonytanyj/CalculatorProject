c502eaef14b5226a9c5a2654bd1c54dea0e5c0c3
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-27 16:08:52 +0800

    Added operate fn

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ function multiply(a,b){
     a_int = parseInt(a);
     b_int = parseInt(b);
 
+    if (b_int === 0){
+        return "ERROR"
+    }
     return a_int * b_int
 }
 
@@ -24,4 +27,28 @@ function divide(a,b){
     b_int = parseInt(b);
 
     return a_int / b_int
+}
+
+function operate(number1, operator, number2) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = addition(number1, number2);
+            break;
+        case '-':
+            result = subtract(number1, number2);
+            break;
+        case '*':
+            result = multiply(number1, number2);
+            break;
+        case '/':
+            if (parseInt(number2) === 0) {
+                return "Cannot divide by zero";
+            }
+            result = divide(number1, number2);
+            break;
+        default:
+            return "Invalid operator";
+    }
+    return result;
 }
