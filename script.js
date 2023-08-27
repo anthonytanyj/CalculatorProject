39b0d01b16c5d0f38b65714adee6fa9b905b375c
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-27 15:58:55 +0800

    Added basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function addition(a,b){
+    a_int = parseInt(a);
+    b_int = parseInt(b);
+
+    return a_int + b_int
+}
+
+function subtract(a,b){
+    a_int = parseInt(a);
+    b_int = parseInt(b);
+
+    return a_int - b_int
+}
+
+function multiply(a,b){
+    a_int = parseInt(a);
+    b_int = parseInt(b);
+
+    return a_int * b_int
+}
+
+function divide(a,b){
+    a_int = parseInt(a);
+    b_int = parseInt(b);
+
+    return a_int / b_int
+}
